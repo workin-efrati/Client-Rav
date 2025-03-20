05c45d034b31f93cc38eb9ed1fb8ad0e34f0efd9
AviadDerli <aviad@workin.co.il>
2025-03-20 20:02:30 +0200

    dates

---
+++ src/helpers/dates.js
@@ -0,0 +1,12 @@
+function formatDate(isoString) {
+    const date = new Date(isoString);
+    const day = String(date.getUTCDate()).padStart(2, '0');
+    const month = String(date.getUTCMonth() + 1).padStart(2, '0');
+    const year = date.getUTCFullYear();
+    const hours = String(date.getUTCHours()).padStart(2, '0');
+    const minutes = String(date.getUTCMinutes()).padStart(2, '0');
+
+    return `${day}-${month}-${year}  ${hours}:${minutes}`;
+}
+
+export default {formatDate}
